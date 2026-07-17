bbb192603d82f7a6cd5b3270554d1fa2160e7112
Wil Edward Munoz <wiledward@Wils-MacBook-Pro.local>
2026-07-17 11:34:17 -0500

    actualizacion VIE17J 3

--- components/forms/ServiceSheetAdminForm.tsx
+++ components/forms/ServiceSheetAdminForm.tsx
@@ -665,60 +665,6 @@ export default function ServiceSheetAdminForm({
       </div>
 
       {/* SAVE */}
-      {showProductModal && (
-
-<div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-
-<div className="w-full max-w-md rounded-[24px] bg-white p-6">
-
-<h2 className="mb-5 text-xl font-bold">
-
-Nuevo Producto
-
-</h2>
-
-<input
-value={newProduct}
-onChange={(e)=>
-setNewProduct(
-e.target.value
-)
-}
-placeholder="Nombre del producto"
-className="w-full rounded-xl border border-gray-200 p-3"
-/>
-
-<div className="mt-6 flex justify-end gap-3">
-
-<button
-type="button"
-onClick={()=>
-setShowProductModal(false)
-}
-className="rounded-xl bg-gray-200 px-5 py-2"
->
-
-Cancelar
-
-</button>
-
-<button
-type="button"
-onClick={saveNewProduct}
-className="rounded-xl bg-black px-5 py-2 text-white"
->
-
-Guardar
-
-</button>
-
-</div>
-
-</div>
-
-</div>
-
-)}
 
       <button
         type="button"
